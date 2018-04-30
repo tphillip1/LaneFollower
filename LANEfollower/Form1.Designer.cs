48d456b5c623882d381abb72cde530afb875954d
Tyler J. Phillips <tphillip1@ltu.edu>
2018-04-30 15:50:33 -0400

    Add project files.

---
+++ LANEfollower/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace LANEfollower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(435, 393);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(485, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(418, 393);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 435);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(258, 435);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 475);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Thresh1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(297, 475);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Thresh2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(571, 440);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Left %";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(671, 438);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Center %";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(812, 440);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Right %";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(452, 440);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 17);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Red %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(915, 532);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ LANEfollower/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System.IO.Ports;
+using System.Diagnostics;
+
+namespace LANEfollower
+{
+    public partial class Form1 : Form
+    {
+        private VideoCapture _capture;
+        private Thread _captureThread;
+        SerialPort _serialPort = new SerialPort("COM5", 2400);
+
+        const byte STOP = 0x7F;
+        const byte FLOAT = 0x0F;
+        const byte FORWARD = 0x6f;
+        const byte BACKWARD = 0x5F;
+        byte left = FORWARD;
+        byte right = FORWARD;
+
+        int t1, t2; //Threshold values
+        int lpercent = 0;//left white count %
+        int cpercent = 0;//center white count %
+        int rpercent = 0;//right white count %
+        int redpercent = 0;//red count %
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _capture = new VideoCapture();
+            _captureThread = new Thread(DisplayWebcam);
+            _captureThread.Start();
+
+            _serialPort.DataBits = 8;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.Two;
+            _serialPort.Open();
+        }
+
+        private void DisplayWebcam()
+        {
+            while (_capture.IsOpened)
+            {
+                Mat frame = _capture.QueryFrame();
+                CvInvoke.Resize(frame, frame, pictureBox1.Size);
+                Image<Gray, Byte> grayImage = frame.ToImage<Gray, Byte>();//gray image
+                Image<Hsv, byte> red = frame.ToImage<Bgr, byte>().Convert<Hsv, byte>(); //change for red
+                Image<Gray, Byte>[] channels = red.Split();
+
+                channels[2] = channels[0].ThresholdBinaryInv(new Gray(65), new Gray(255));
+                channels[2] &= channels[1].ThresholdBinary(new Gray(65), new Gray(255));
+
+                grayImage = grayImage.ThresholdBinary(new Gray(t1), new Gray(t2));
+
+                int redCount = 0;
+                for (int x = 0; x < grayImage.Width; x++)
+                {
+                    for (int y = 0; y < grayImage.Height; y++)
+                    {
+                        if (channels[2].Data[y, x, 0] == 255)
+                            redCount++;
+                    }
+                }
+                redpercent = (redCount * 100) / (grayImage.Width * grayImage.Height);//converts red count into a %
+
+                label6.Invoke(new Action(() => label6.Text = redpercent.ToString()));//outputs red % to screen
+
+                int leftWhiteCount = 0;
+                for (int x = 0; x < grayImage.Width / 3; x++)//calculates white pixals in the left (1/3) of screen
+                {
+                    for (int y = 0; y < grayImage.Height; y++)
+                    {
+                        if (grayImage.Data[y, x, 0] == 255)
+                            leftWhiteCount++;
+                    }
+                }
+                lpercent = (leftWhiteCount * 100) / ((grayImage.Width / 3) * grayImage.Height);//converts left white count to a %
+
+                label3.Invoke(new Action(() => label3.Text = lpercent.ToString()));//outputs left % to screen
+
+                int centerWhiteCount = 0;
+                for (int x = grayImage.Width / 3; x < (2 * grayImage.Width) / 3; x++)//calculates white pixals in the center (2/3) of screen
+                {
+                    for (int y = 0; y < grayImage.Height; y++)
+                    {
+                        if (grayImage.Data[y, x, 0] == 255)
+                            centerWhiteCount++;
+                    }
+                }
+                cpercent = (centerWhiteCount * 100) / ((grayImage.Width / 3) * grayImage.Height);//converts center white count to a %
+
+                label4.Invoke(new Action(() => label4.Text = cpercent.ToString()));//outputs center % to screen
+
+                int rightWhiteCount = 0;
+                for (int x = (2 * grayImage.Width) / 3; x < grayImage.Width; x++)//calculates white pixals in the right (3/3) of screen
+                {
+                    for (int y = 0; y < grayImage.Height; y++)
+                    {
+                        if (grayImage.Data[y, x, 0] == 255)
+                            rightWhiteCount++;
+                    }
+                }
+                rpercent = (rightWhiteCount * 100) / ((grayImage.Width / 3) * grayImage.Height);//converts right white count to a %
+
+                label5.Invoke(new Action(() => label5.Text = rpercent.ToString()));//outputs right % to screen
+                
+                if (redpercent > 13)
+                {
+                    byte[] buffer = { 0x01, FORWARD, BACKWARD };
+                    _serialPort.Write(buffer, 0, 3);
+
+                    Thread.Sleep(1500);//this stopped the program to allow the robot to turn       
+                }
+                             
+                if (leftWhiteCount > centerWhiteCount & rightWhiteCount > centerWhiteCount)//lpercent > 10 & rpercent > 10)
+                {
+                    byte[] buffer = { 0x01, FORWARD, FORWARD };//Go straight when both left and right are close or the same white count
+                    _serialPort.Write(buffer, 0, 3);
+                }
+                else if (leftWhiteCount > rightWhiteCount)//lpercent > rpercent)
+                {
+                    byte[] buffer = { 0x01, STOP, FORWARD };//Go right when it is too far to the left
+                    _serialPort.Write(buffer, 0, 3);
+                }
+
+                else if (rightWhiteCount > leftWhiteCount)//rpercent > lpercent)
+                {
+                    byte[] buffer = { 0x01, FORWARD, STOP };//Go left when it is too far to the right
+                    _serialPort.Write(buffer, 0, 3);
+                }
+                
+                pictureBox1.Image = grayImage.ToBitmap();//gray image
+                pictureBox2.Image = channels[2].ToBitmap();//red image
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosedEventArgs e)
+        {
+            _captureThread.Abort();//this closes the webcam after closing the form box. Stops the code from running
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            t1 = int.Parse(textBox1.Text);//input threshold 1
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            t2 = int.Parse(textBox2.Text);//input threshold 2
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
